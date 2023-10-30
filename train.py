093fac0c62113382bddf95f2e337cfcd6cab25e6
Your Name <you@example.com>
2023-10-30 19:41:49 +0800

    add js dataset

--- train.py
+++ train.py
@@ -261,12 +261,12 @@ def get_parser():
     
     parser.add_argument(
         "--train_dir",
-        default='MyTTSDataset/small_tune'
+        default='JS_Dataset/train_tune'
     )
     
     parser.add_argument(
         "--valid_dir",
-        default='MyTTSDataset/small_tune'
+        default='JS_Dataset/valid_tune'
     )
 
     add_model_arguments(parser)
